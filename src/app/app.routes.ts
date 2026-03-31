dfb20a94c27c05b7436dd0c9e09eec01bd6a6c08
Samantha Gayle Oliveros <gayleoliveros2016@gmail.com>
2026-03-31 22:42:36 +0800

    Feature: added admin profile, and connected admin sidebar to it.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
+import { AdminProfileComponent } from './admin-profile/admin-profile.component';
 import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent }, // default route
+  { path: 'admin-profile', component: AdminProfileComponent } //admin profile
   // other routes here if needed
 ];
