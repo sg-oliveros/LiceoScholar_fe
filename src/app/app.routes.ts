65c07630d69b98a05eb5fc71d030990816c79322
Christy Reyes <careyes04249@liceo.edu.ph>
2026-04-02 01:18:25 +0800

    added search filter

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,13 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { AdminProfileComponent } from './admin-profile/admin-profile.component';
+import { StudentListComponent } from './admin-student-list-filter/admin-student-filter.component';  
+import { AdminSidebarComponent } from './admin-sidebar/admin-sidebar.component';
+
 
 export const routes: Routes = [
   { path: '', component: LoginComponent }, // default route
-  // other routes here if needed
+  { path: 'admin-students-list', component: StudentListComponent },
+  { path: 'admin-profile', component: AdminProfileComponent }, //admin profile
+  { path: 'admin', component: AdminSidebarComponent} // path for the sidebar
 ];
