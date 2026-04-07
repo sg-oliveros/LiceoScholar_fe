cf0736f206db2ae651a92030aca2f68a6c06e181
Samantha Gayle Oliveros <gayleoliveros2016@gmail.com>
2026-04-08 00:22:21 +0800

    fixed minor issues after the merge

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from "@angular/common";
 import { Component, inject, signal } from "@angular/core";
 import { Router } from "@angular/router";
+import { ForgetPassComponent } from "./components/forget-pass/forget-pass.component";
 import { LoginFormComponent } from "./components/login-form/login-form.component";
 import { SignupFormComponent } from "./components/signup-form/signup-form.component";
 import { SwitchFormComponent } from "./components/switch/switch-form.component";
@@ -10,7 +11,7 @@ import { SwitchFormComponent } from "./components/switch/switch-form.component";
     standalone: true,
     templateUrl: './login.component.html',
     styleUrls: ['./login.component.scss'],
-    imports: [LoginFormComponent, SignupFormComponent, CommonModule, SwitchFormComponent]
+    imports: [LoginFormComponent, SignupFormComponent, CommonModule, SwitchFormComponent, ForgetPassComponent]
 })
 export class LoginComponent {
     private router = inject(Router);
@@ -18,7 +19,7 @@ export class LoginComponent {
     public email = signal<string>('example@email.com');
     public password = signal<string>('password123');
 
-    public mode = signal<'login' | 'signup'>('login');
+    mode = signal<'login' | 'signup' | 'forget'>('login');
 
 
     public onModeChange(mode: 'login' | 'signup') {
